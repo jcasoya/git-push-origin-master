a922b303c141020e18f6af42c37430ae58a40189
Jorge Cases <jcasoya@gmail.com>
2021-07-10 16:01:21 +0200

    show action done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
-
+get '/tasks/:id', to: 'tasks#show'
   post '/tasks', to: 'tasks#create'
 
   get '/tasks/new', to: 'tasks#new'
 
   get 'tasks/:id/edit', to: 'tasks#edit'
 
-  get '/tasks/:id', to: 'tasks#show'
+
 
   patch '/tasks/:id', to: 'tasks#update'
 
